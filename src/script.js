26cb7c24f0f441ae5101eba9fa19ff004474f787
Samm005 <samudyathakaranth@gmail.com>
2025-11-24 17:24:14 +0530

    The basic functions required in js to provide functionality

---
+++ src/script.js
@@ -0,0 +1,20 @@
+const WeatherForm = document.getElementById("Weather");
+const cityName = document.getElementById("cityName");
+const display = document.getElementById("displayCard");
+const apiKey = "eb633ce525f89f1c11e0709bceb0108d";
+
+WeatherForm.addEventListener("submit", (event) => {});
+
+async function weatherData(city) {}
+
+function displayweather(data) {}
+
+function weatherEmoji(weatherId) {}
+
+function displayError(msg) {}
+
+function ToggleTemp() {}
+
+function Dropdownmenu() {}
+
+function CurrentLoc() {}
